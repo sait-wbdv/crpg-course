d49e5dd9af2d2db75b791f3099160da2141a8e58
Ashlyn Knox <ashlyn@lilyx.io>
2024-01-10 15:51:20 -0700

    module setup in config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  modules: ["@nuxtjs/supabase", "@nuxt/content", "@nuxt/ui"],
+  ui: {
+    global: true,
+    icons: ["fa6-brands", "fa6-solid", "fa6-regular"],
+  },
+});
