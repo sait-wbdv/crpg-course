8c3ba4d111ea2e0ea8e608c13011a86f06bb7aff
Ashlyn Knox <ashlyn@lilyx.io>
2024-01-11 11:21:48 -0700

    user logout page

---
+++ pages/logout.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <button @click="logout">Sign out</button>
+    <p>{{ user }}</p>
+  </div>
+</template>
+
+<script setup>
+const user = useSupabaseUser();
+const supabase = useSupabaseClient();
+
+const logout = () => {
+  try {
+    const { error } = supabase.auth.signOut();
+    if (error) {
+      throw error;
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
+</script>
