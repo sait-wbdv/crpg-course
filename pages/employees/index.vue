27ca048950b45f1438ddfd9e9374964d32c6da35
Ashlyn Knox <ashlyn@lilyx.io>
2024-01-11 16:44:54 -0700

    client side data fetch

--- pages/employees/index.vue
+++ pages/employees/index.vue
@@ -6,8 +6,9 @@
     </header>
     <div>
       <ul>
+        <!-- currently this pulls from the table in the api. to be changed to supabase -->
         <li v-for="employee in employees" :key="employee.id">
-          loop through the api to generate links to all the employee pages
+          {{ employee.LAST_NAME }} {{ employee.FIRST_NAME }}
         </li>
       </ul>
     </div>
@@ -18,8 +19,13 @@
 definePageMeta({
   middleware: ["auth"],
 });
-const { users } = await useFetch("/api/users");
-const { employees } = await useFetch("/api/employees");
-console.log(`users: ${users}`);
+const employees = ref([]);
+async function getEmployees() {
+  const { data } = await useSupabaseClient().from("Employees").select();
+  employees.value = data;
+}
+onMounted(() => {
+  getEmployees();
+});
 console.log(employees);
 </script>
