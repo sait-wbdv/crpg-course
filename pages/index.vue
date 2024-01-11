d3240977d9a90427307936f09cd85ef9544a48c9
Ashlyn Knox <ashlyn@lilyx.io>
2024-01-11 12:55:39 -0700

    protected route middle ware initial example

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,28 @@
+<script setup>
+// set this up to be more maintainable
+definePageMeta({
+  middleware: [
+    function (to, from) {
+      const user = useSupabaseUser();
+      if (user.value) {
+        return;
+      }
+      return navigateTo("/login");
+    },
+  ],
+});
+</script>
 <template>
-  <div>Index Page</div>
-  <NuxtLink to="/login">Login Page</NuxtLink>
+  <main>
+    <header>
+      <h1>CRPG 206</h1>
+      <p>Winter 2024</p>
+    </header>
+    <section>
+      <header>
+        <h2>Course Calendar</h2>
+      </header>
+      <div></div>
+    </section>
+  </main>
 </template>
-
-<script setup></script>
-
-<style lang="scss" scoped></style>
